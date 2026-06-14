2b8a4834f7bbbde3dbb53640459463feed50b89b
maki20ei24 <makiei202301@gmail.com>
2026-06-15 00:52:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080; // OpenShiftの標準ポート8080
 
 app.get('/', (req, res) => {
-    res.send('<h1>🚀 Webhookによる自動トリガー成功！ V1</h1>');
+    res.send('<h1> Webhookによる自動トリガー成功！ V1</h1>');
 });
 
 app.listen(PORT, () => {
